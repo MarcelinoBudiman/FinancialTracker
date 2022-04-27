f8703e4aeac717e6af158f99578430f4eac245ec
Marcelino Budiman <marcelinobudiman47@gmail.com>
2022-04-27 20:23:33 +0700

    Initial Commit

---
+++ NC1_FinanceTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC1_FinanceTrackerApp
+//
+//  Created by Marcelino Budiman on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
