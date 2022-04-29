d42cbc17e6d10829e970501fc4177822a1d8db56
Marcelino Budiman <marcelinobudiman47@gmail.com>
2022-04-29 15:46:45 +0700

    add date

--- NC1_FinanceTrackerApp/TransactionModel.swift
+++ NC1_FinanceTrackerApp/TransactionModel.swift
@@ -12,17 +12,30 @@ struct Transaction{
     var total: Int
     var category: String
     var notes: String?
-    var date: String
+    var date: Date?
     var type: String
+}
+ 
+extension Date {
+
+    static func from(year: Int, month: Int, day: Int) -> Date? {
+        let calendar = Calendar(identifier: .gregorian)
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        dateComponents.timeZone = TimeZone(abbreviation: "WIB")
+        return calendar.date(from: dateComponents) ?? nil
+    }
 }
 
 struct TransactionFeeder{
     func seedTransaction() -> [Transaction] {
         return [
-            Transaction(total: 30000, category: "Food", notes: "Ayam Gepuk", date: "22-10-2022", type: "Expenses"),
-            Transaction(total: 200000, category: "Transportation", notes: "Bensin", date: "20-10-2022", type: "Expenses"),
-            Transaction(total: 500000, category: "Shopping", notes: "Baju HnM", date: "15-10-2022", type: "Expenses"),
-            Transaction(total: 2500000, category: "Salary", notes: "Gajian", date: "22-10-2022", type: "Income")
+            Transaction(total: 30000, category: "Food", notes: "Ayam Gepuk", date: Date.from(year: 2022, month: 10, day: 22) ?? nil, type: "Expenses"),
+            Transaction(total: 200000, category: "Transportation", notes: "Bensin", date: Date.from(year: 2022, month: 10, day: 20) ?? nil, type: "Expenses"),
+            Transaction(total: 500000, category: "Shopping", notes: "Baju HnM", date: Date.from(year: 2022, month: 10, day: 15) ?? nil, type: "Expenses"),
+            Transaction(total: 2500000, category: "Salary", notes: "Gajian", date: Date.from(year: 2022, month: 10, day: 22) ?? nil, type: "Income")
         ]
     }
 }
